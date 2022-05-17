79d02ca2f4f2d1e08510250baba4192dac0a8183
Adarshak777 <103920578+Adarshak777@users.noreply.github.com>
2022-05-17 19:06:42 +0530

    Update Program.cs

--- codetest_2/codetest_2/Program.cs
+++ codetest_2/codetest_2/Program.cs
@@ -51,4 +51,42 @@ namespace codetest_2
             }
         }
   }
+2.
+select DATEDIFF(dd,'2011/05/12','2021/2/9') AS DateDiff;
+
+
+
+3. 
+create database temp8
+
+create table EMP8
+(
+EMPId int primary key,
+Name varchar(25)not null,
+salary int,
+Deptno int
+)
+
+
+
+select * from emp8
+
+
+
+a. accept only 10,20,30 as deptno
+
+
+
+alter table emp8
+add constraint colcheck check( deptno between 10 and 30)
+
+
+
+b. Every employee salary must be in the range of 5000 to 50000 only
+alter table emp8
+add constraint salcheck check( salary between 5000 and 50000) 
+
+
+
+
 
