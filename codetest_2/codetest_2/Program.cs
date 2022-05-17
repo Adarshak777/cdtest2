4f2614bad003910b4dc694e3cbf75196f5e5f0a3
Adarsh Kumar <adarsh.kumar@zensar.com>
2022-05-17 18:48:12 +0530

    codetest2

---
+++ codetest_2/codetest_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codetest_2
+{
+    delegate int calculator (int x, int y);
+    
+    
+
+        class TestDelegate
+        {
+            static int num = 10;
+
+            public static int Addition(int a, int b)
+            {
+                num = a+b;
+                return num;
+            }
+             public static int Subtractin(int q, int r)
+             {
+                num = q - r;
+                return num;
+             }
+           public static int Multiplication(int c, int d)
+            {
+                num = c*d ;
+                return num;
+            }
+            public static int getNum()
+            {
+                return num;
+            }
+            static void Main(string[] args)
+            {
+
+                    calculator ak1 = new calculator(Addition);
+                    calculator ak2 = new calculator(Subtractin);
+                    calculator ak3 = new calculator(Multiplication);
+
+           
+                   ak1(16,2);
+                   Console.WriteLine("addition value is {0} ", getNum());
+                   ak2(2,4);
+                   Console.WriteLine("substraction value is {0} ", getNum());
+                   ak2(12,2);
+                   Console.WriteLine("multiplication value is {0} ", getNum());
+                   Console.ReadKey();
+            }
+        }
+  }
+
